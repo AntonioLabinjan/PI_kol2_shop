2a1423033dd88dc2e861a34365f3602c9003c588
Antonio Labinjan <alabinjan6@gmail.com>
2025-05-27 19:06:06 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia' // 👈 uvoz Pinie
+import { createPinia } from 'pinia' 
 
 const app = createApp(App)
 
-app.use(createPinia()) // 👈 registracija Pinie
+app.use(createPinia()) 
 app.use(router)
 
 app.mount('#app')
